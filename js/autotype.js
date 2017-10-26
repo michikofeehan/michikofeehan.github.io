969d9601e2330edb1d21082dfee2752fc8972d3f
Michiko <michiko.feehan@gmail.com>
2017-10-26 01:19:49 -0400

    spellings

--- js/autotype.js
+++ js/autotype.js
@@ -1,6 +1,6 @@
 var i = 1;
 var j = -1;
-var textList = ['BiomedicalEngineer.JazzSaxophonist.Programmer'];
+var textList = ['Biomedical Engineer.Jazz Saxophonist.Programmer '];
 var a = textList[0];
 var len = textList.length;
 var num = 0;
